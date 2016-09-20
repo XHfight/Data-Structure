71fa8e4c83b2e0b542da81d8455a476f4418318e
XHfight <X_H_fight@163.com>
2016-09-20 14:27:48 +0800

    Test

---
+++ Matrix/Test.cpp
@@ -0,0 +1,36 @@
+#include "SymmetricMatrix.h"
+#include "SparseMatrix.h"
+void TestSymmetricMatrix()
+{
+	 int a [5][5]=  
+	 {
+		 {0,1,2,3,4},
+		 {1,0,1,2,3},
+		 {2,1,0,1,2},
+		 {3,2,1,0,1},
+		 {4,3,2,1,0},
+	 };
+
+	SymmetricMatrix<int> sm((int*)a, 5);
+	sm.Display();
+}
+
+void TestSparseMatrix()
+{
+	int array [6][5] =     
+	{{1, 0, 3, 0, 5},
+	{0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0},
+	{1, 0, 3, 0, 5},
+	{0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0}};
+	SparseMatrix<int> sm((int *)array,6,5,0);
+	sm.Display();
+}
+int main()
+{
+	//TestSymmetricMatrix();
+	TestSparseMatrix();
+	system("pause");
+	return 0;
+}
