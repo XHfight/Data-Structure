5971f31229112966b782bce0222358209155feaa
XHfight <X_H_fight@163.com>
2016-10-12 20:07:51 +0800

    利用仿函数实现堆

--- Heap/Heap.h
+++ Heap/Heap.h
@@ -1,15 +1,32 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <cassert>
 using namespace std;
 
-
+template <class T>
+struct CmpGreat   //建大堆
+{
+	bool operator()(const T& data1, const T& data2)
+	{
+		return data1>data2;
+	}
+};
 
 template<class T>
-class MaxHeap
+struct CmpLess   //建小堆
+{
+	bool operator()(const T& data1, const T& data2)
+	{
+		return data1<data2;
+	}
+};
+
+template<class T, class CMP = CmpGreat<T>>
+class Heap
 {
 public:
-	MaxHeap(T* arr, size_t size)
+	Heap(T* arr, size_t size)
 	{
 		for(size_t i=0; i<size; ++i)
 		{
@@ -33,11 +50,11 @@ public:
 
 	void Pop()
 	{
-		size_t size = _heap.size();
+		assert(!_heap.empty());
 
+		size_t size = _heap.size();
 		swap(_heap[0], _heap[size-1]);
 		_heap.pop_back();
-
 		_AdjustDown(0);
 	}
 protected:
@@ -51,12 +68,14 @@ protected:
 
 		while(child <= size)
 		{
-			if(child+1 < size && _heap[child] < _heap[child+1])
+			//if(child+1 < size && _heap[child] < _heap[child+1])
+			if(child+1 < size && CMP()(_heap[child+1],_heap[child]))
 			{
 				child++;
 			}
 
-			if(_heap[parent] < _heap[child])
+			//if(_heap[parent] < _heap[child])
+			if(CMP()(_heap[child], _heap[parent]))
 			{
 				swap(_heap[parent], _heap[child]);
 				parent = child;
@@ -74,7 +93,8 @@ protected:
 		while(child > 0)
 		{
 			size_t parent = (child-1)/2;
-			if(_heap[child] > _heap[parent])
+			//if(_heap[child] > _heap[parent])
+			if(CMP()(_heap[child],_heap[parent]))
 			{
 				swap(_heap[child], _heap[parent]);
 			}
--- Heap/Test.cpp
+++ Heap/Test.cpp
@@ -2,8 +2,8 @@
 
 void TestMaxHeap()
 {
-	int a [] = {10,11, 13, 12, 16, 18, 15, 17, 14, 19};
-	MaxHeap<int> h1(a, sizeof(a)/sizeof(a[0]));
+	int a[] = {10,11, 13, 12, 16, 18, 15, 17, 14, 19};
+	Heap<int, CmpLess<int>> h1(a, sizeof(a)/sizeof(a[0]));
 	h1.Push(30);
 	h1.Pop();
 }
