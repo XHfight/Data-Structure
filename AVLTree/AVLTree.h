d98f6c45f8f55a93705882e8824597ab381b10ed
XHfight <X_H_fight@163.com>
2016-10-26 01:42:05 +0800

    AVLTree 修改双旋

--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -1,9 +1,9 @@
 /*************************************************************************
-	> File Name: AVLTree.h
-	> Author: XH
-	> Mail: X_H_fight@163.com 
-	> Created Time: Sun 23 Oct 2016 05:10:21 PM CST
- ************************************************************************/
+> File Name: AVLTree.h
+> Author: XH
+> Mail: X_H_fight@163.com 
+> Created Time: Sun 23 Oct 2016 05:10:21 PM CST
+************************************************************************/
 
 #pragma once
 #include<iostream>
@@ -17,15 +17,15 @@ struct AVLTreeNode
 	AVLTreeNode<K, V>* _left;
 	AVLTreeNode<K, V>* _right;
 	AVLTreeNode<K, V>* _parent;
-	int _ba; //平衡因子
+	int _bf; //平衡因子
 
 	AVLTreeNode(const K& key, const V& value)
 		:_key(key)
-		 ,_value(value)
-		 ,_left(NULL)
-		 ,_right(NULL)
-		 ,_parent(NULL)
-		 ,_ba(0)
+		,_value(value)
+		,_left(NULL)
+		,_right(NULL)
+		,_parent(NULL)
+		,_bf(0)
 	{}
 };
 
@@ -37,7 +37,7 @@ public:
 	AVLTree()
 		:_root(NULL)
 	{}
-	
+
 	bool Insert(const K& key, const V& value)
 	{
 		if(_root == NULL)
@@ -45,7 +45,7 @@ public:
 			_root = new Node(key, value);
 			return true;
 		}
-		
+
 		//查找插入位置
 		Node* parent = NULL; 
 		Node* cur = _root;
@@ -64,95 +64,24 @@ public:
 			else
 				return false;
 		}
-		
+
 		//插入
 		cur = new Node(key, value);
 		if(parent->_key > key) //parent的左插入
 		{
 			parent->_left = cur;
 			cur->_parent = parent;
-			//改平衡因子
-			--parent->_ba;
 
-			//检查平衡因子
-			while(parent)
-			{
-				if(parent->_ba == 0)//平衡因子为0，停止
-					break;
-				else if(parent->_ba == 1 ||parent->_ba == -1)//平衡因子为1/-1，继续向父结点调整
-				{
-					parent = parent->_parent;
-					if(parent)
-						--parent->_ba;
-				}
-				else//平衡因子为2/-2，旋转
-				{
-					if(parent->_ba==2 && parent->_right->_ba==1)
-					{
-						//左旋
-						_RotateL(parent);
-					}
-					else if(parent->_ba==2 && parent->_right->_ba==-1)
-					{
-						//右左双旋
-						_RotateRL(parent);
-					}
-					else if(parent->_ba==-2 && parent->_left->_ba==-1)
-					{
-						//右旋
-						_RotateR(parent);
-					}
-					else //parent->_ba==-2 && parent->_left->_ba==1
-					{
-						//左右双旋
-						_RotateLR(parent);
-					}
-					break;
-				}	
-			}
+			//调整平衡因子
+			--parent->_bf;
+			_AdjustTree(parent);
 		}
 		else	//parent的右插入
 		{
 			parent->_right = cur;
 			cur->_parent = parent;
-			++parent->_ba;
-			//检查平衡因子
-			while(parent)
-			{
-				if(parent->_ba == 0)//平衡因子为0，停止
-					break;
-				else if(parent->_ba == 1 ||parent->_ba == -1)//平衡因子为1/-1，继续向父结点调整
-				{
-					parent = parent->_parent;
-					if(parent)
-						++parent->_ba;
-				}
-				else//平衡因子为2/-2，旋转
-				{
-					if(parent->_ba==2 && parent->_right->_ba==1)
-					{
-						//左旋
-						_RotateL(parent);
-					}
-					else if(parent->_ba==2 && parent->_right->_ba==-1)
-					{
-						//右左双旋
-						_RotateRL(parent);
-					}
-					else if(parent->_ba==-2 && parent->_left->_ba==-1)
-					{
-						//右旋
-						_RotateR(parent);
-					}
-					else
-					{
-						//左右双旋
-						_RotateLR(parent);
-					}
-					break;
-				}
-			}
-
+			++parent->_bf;
+			_AdjustTree(parent);
 		}
 		return true;
 	}
@@ -160,6 +89,12 @@ public:
 	void InOrder()
 	{
 		_InOrder(_root);
+		cout << endl;
+	}
+	
+	bool IsBalance()
+	{
+		return _IsBalance(_root);
 	}
 
 	~AVLTree()
@@ -176,6 +111,51 @@ protected:
 
 		delete root;
 		root = NULL;
+	}
+	void _AdjustTree(Node* parent)
+	{
+		//检查平衡因子
+		while(parent)
+		{
+			if(parent->_bf == 0)//平衡因子为0，停止
+				break;
+			else if(parent->_bf == 1 ||parent->_bf == -1)//平衡因子为1/-1，继续向父结点调整
+			{
+				Node* sub = parent;
+				parent = parent->_parent;
+				if(parent)
+				{
+					if(parent->_left == sub)
+						--parent->_bf;
+					else
+						++parent->_bf;
+				}
+			}
+			else//平衡因子为2/-2，旋转
+			{
+				if(parent->_bf==2 && parent->_right->_bf==1)
+				{
+					//左旋
+					_RotateL(parent);
+				}
+				else if(parent->_bf==2 && parent->_right->_bf==-1)
+				{
+					//右左双旋
+					_RotateRL(parent);
+				}
+				else if(parent->_bf==-2 && parent->_left->_bf==-1)
+				{
+					//右旋
+					_RotateR(parent);
+				}
+				else //parent->_ba==-2 && parent->_left->_ba==1
+				{
+					//左右双旋
+					_RotateLR(parent);
+				}
+				break;
+			}	
+		}
 	}
 	//右旋
 	void _RotateR(Node* parent)
@@ -209,7 +189,7 @@ protected:
 		parent->_parent = subL;
 
 		//改平衡因子
-		subL->_ba = parent->_ba = 0;
+		subL->_bf = parent->_bf = 0;
 	}
 	void _RotateL(Node* parent)
 	{
@@ -241,19 +221,60 @@ protected:
 		parent->_parent = subR;
 
 		//改平衡因子
-		subR->_ba = parent->_ba = 0;
+		subR->_bf = parent->_bf = 0;
 	}
 
 	void _RotateRL(Node* parent)
 	{
+		Node* subR = parent->_right;
+		Node* subRL = subR->_left;
+		int bf = subRL->_bf;
+
 		_RotateR(parent->_right);
 		_RotateL(parent);
+
+		if(bf == 0)
+		{
+			parent->_bf = subR->_bf = subRL->_bf;
+		}
+		else if(bf == 1)
+		{
+			parent->_bf = -1;
+			subRL->_bf = 0;
+			subR->_bf = 0;
+		}
+		else
+		{
+			parent->_bf = 0;
+			subRL->_bf = 0;
+			subR->_bf = 1;
+		}
 	}
 
 	void _RotateLR(Node* parent)
 	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+		int bf = subLR->_bf;
 		_RotateL(parent->_left);
 		_RotateR(parent);
+
+		if(bf == 0)
+		{
+			subLR->_bf = subL->_bf = parent->_bf = 0;
+		}
+		else if(bf == 1)
+		{
+			parent->_bf = 0;
+			subLR->_bf = 0;
+			subL->_bf = -1;
+		}
+		else
+		{
+			parent->_bf = 1;
+			subLR->_bf = 0;
+			subL->_bf = 0;
+		}
 	}
 
 	void _InOrder(Node* root)
@@ -263,6 +284,31 @@ protected:
 		_InOrder(root->_left);
 		cout << root->_key << " ";
 		_InOrder(root->_right);
+	}
+
+	size_t _Height(Node* root)
+	{
+		if(root == NULL)
+			return 0;
+		size_t LHeight = _Height(root->_left);
+		size_t RHeight = _Height(root->_right);
+
+		return (LHeight>RHeight) ? (LHeight+1) : (RHeight+1);
+	}
+
+	bool _IsBalance(Node* root)
+	{
+		if(root == NULL)
+			return true;
+
+		int bf = _Height(root->_right)-_Height(root->_left);
+		if(bf != root->_bf)
+			cout << "平衡因子异常 key:"<<root->_key<< endl;
+
+		if(abs(bf) < 2)
+			return _IsBalance(root->_left) && _IsBalance(root->_right);
+		else
+			return false;
 	}
 protected:
 	Node* _root;
--- AVLTree/Test.cpp
+++ AVLTree/Test.cpp
@@ -9,19 +9,36 @@
 #include"AVLTree.h"
 using namespace std;
 
-void TestAVLTree()
+//测试用例一
+void TestAVLTree1()
 {
 	int arr[] = {16, 3, 7, 11, 9, 26, 18, 14, 15};
-	AVLTree<int, int> t;
+	AVLTree<int, int> t1;
 	for(int i = 0; i<sizeof(arr)/sizeof(arr[0]); ++i)
 	{
-		t.Insert(arr[i], i);
+		t1.Insert(arr[i], i);
 	}
-	t.InOrder();
+	t1.InOrder();
+	cout<<"IsBalance() ? "<<t1.IsBalance()<<endl;
+}
+
+//测试用例二
+void TestAVLTree2()
+{
+	int arr[] = {4, 2, 6, 1, 3, 5, 15, 7, 16, 14};
+	AVLTree<int, int> t2;
+	for(int i = 0; i<sizeof(arr)/sizeof(arr[0]); ++i)
+	{
+		t2.Insert(arr[i], i);
+	}
+	t2.InOrder();
+	cout<<"IsBalance() ? "<<t2.IsBalance()<<endl;
 }
 
 int main()
 {
-	TestAVLTree();
+	TestAVLTree1();
+	cout << endl;
+	TestAVLTree2();
 	return 0;
 }
