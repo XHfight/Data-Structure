380bb63be5dd204a5d5e0499104cdd6a59f6d3a6
XHfight <X_H_fight@163.com>
2016-11-08 11:50:18 +0800

    处理Top函数当size为空的情况

--- Heap/Heap.h
+++ Heap/Heap.h
@@ -26,6 +26,8 @@ template<class T, class CMP = CmpGreat<T>>
 class Heap
 {
 public:
+	Heap()
+	{}
 	Heap(T* arr, size_t size)
 	{
 		for(size_t i=0; i<size; ++i)
@@ -57,6 +59,16 @@ public:
 		_heap.pop_back();
 		_AdjustDown(0);
 	}
+
+	size_t Size() const
+	{
+		return _heap.size();
+	}
+
+	const T& Top() const
+	{
+		return _heap[0];
+	}
 protected:
 	//向下调整
 	void _AdjustDown(size_t i)
@@ -66,7 +78,7 @@ protected:
 
 		size_t size = _heap.size();
 
-		while(child <= size)
+		while(child < size)
 		{
 			//if(child+1 < size && _heap[child] < _heap[child+1])
 			if(child+1 < size && CMP()(_heap[child+1],_heap[child]))
@@ -107,49 +119,3 @@ protected:
 protected:
 	vector<T> _heap;
 };
-
-
-template<class T>
-void AdjustDown(T* a, size_t size, size_t root)
-{
-	size_t parent = root;
-	size_t child = root*2+1;
-
-	while(child < size)
-
-	{
-		if(child+1<size && a[child+1]<a[child])
-		{
-			child++;
-		}
-		if(a[child] < a[parent])
-		{
-			swap(a[child], a[parent]);
-			parent = child;
-			child = parent*2+1;
-		}
-		else
-			break;
-	}
-
-}
-//降序 --》小堆
-//时间复杂度：N*LgN
-template<class T>
-void HeapSort(T* arr, size_t size)
-{
-	//建堆
-	for(int i = (size-2)/2; i>=0; --i)
-	{
-		AdjustDown(arr, size, i);	
-	}
-	
-	//排序
-	int index = size-1;  //index:记录当前排序的结点
-	while(index)
-	{ 
-		swap(arr[0], arr[index]);
-		AdjustDown(arr, --index, 0);
-	}
-
-}
